d56808e53846f4bd8b1cd1b3342a57b258962309
MrHidalgo <Luckyhidalgo@gmail.com>
2017-03-02 16:25:23 +0200

    FIx promo style files Add new name for files in server.js

--- server.js
+++ server.js
@@ -5,8 +5,29 @@ app.set('port',	(process.env.PORT	||	11112));
 
 
 // MAIN PAGE
-app.use('/',	        express.static('./dist/', {
+app.use('/',            express.static('./dist/', {
         'index' : 'index.html'
+}));
+app.use('/category',    express.static('./dist/', {
+        'index' : 'category.html'
+}));
+app.use('/cart',        express.static('./dist/', {
+        'index' : 'cart.html'
+}));
+app.use('/product',     express.static('./dist/', {
+        'index' : 'product.html'
+}));
+app.use('/track',       express.static('./dist/', {
+        'index' : 'track.html'
+}));
+app.use('/support',     express.static('./dist/', {
+        'index' : 'support.html'
+}));
+app.use('/policies',    express.static('./dist/', {
+        'index' : 'policies.html'
+}));
+app.use('/faq',         express.static('./dist/', {
+        'index' : 'faq.html'
 }));
 
 
